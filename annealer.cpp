d0c2fcb16b775e75a24613783aac66ab2a62d405
Ishan Goel <igoel.mail@gmail.com>
2024-09-23 03:25:25 -0400

    get energy diff instead of eval each time: 33% faster

--- annealer.cpp
+++ annealer.cpp
@@ -13,7 +13,6 @@ using namespace std;
 
 using solution_t = vector<bool>;
 using qubo_t = map<pair<int, int>, double>;
-// using scheduler_t = double (*)(double T_0, double T, int iter, int max_iter);
 using scheduler_t = function<double(double T_0, double T, int iter, int max_iter)>;
 
 ostream& operator << (ostream& os, const solution_t& x) {
@@ -49,6 +48,68 @@ double evaluate(const solution_t& x, const qubo_t& Q) {
     return value;
 }
 
+double evaluateDiff(const solution_t& x, const qubo_t& Q, int flip_idx) {
+    double diff = 0.0;
+    for (const auto& entry : Q) {
+        if (entry.first.first == flip_idx || entry.first.second == flip_idx) {
+            if (x[entry.first.first] && x[entry.first.second]) {
+                // both on, flipping either will turn it off.
+                diff -= entry.second;
+            } else if (x[entry.first.first] || x[entry.first.second]) {
+                // one on. flip it on if the one we're flipping is off.
+                if (entry.first.first == flip_idx && !x[entry.first.first]) {
+                    diff += entry.second;
+                } else if (entry.first.second == flip_idx && !x[entry.first.second]) {
+                    diff += entry.second;
+                }
+            } else {
+                // both off. flip it on only if both are on.
+                if (entry.first.first == flip_idx && entry.first.second == flip_idx) {
+                    diff += entry.second;
+                }
+            }
+        }
+    }
+    return diff;
+
+    // for (int i = 0; i < x.size(); i++) {
+    //     // double coupling = Q[{flip_idx, i}] + Q[{i, flip_idx}];
+    //     // Q is const, so we can't use the above line.
+    //     double coupling = 0;
+    //     auto it = Q.find({flip_idx, i});
+    //     if (it != Q.end()) {
+    //         coupling += it->second;
+    //     }
+    //     if (i != flip_idx) {
+    //         it = Q.find({i, flip_idx});
+    //         if (it != Q.end()) {
+    //             coupling += it->second;
+    //         }
+    //     }
+
+    //     if (coupling != 0) {
+    //         bool i_on = x[i];
+    //         bool flip_on = x[flip_idx];
+    //         if (i_on && flip_on) {
+    //             // currently on. flipping either will turn it off.
+    //             diff -= coupling;
+    //         } else if (i_on || flip_on) {
+    //             // one of them is on. 
+    //             // both will become on if the one we're flipping is off.
+    //             if (!flip_on) {
+    //                 diff += coupling;
+    //             }
+    //         } else {
+    //             // both off. flip it on only if both will become on.
+    //             if (i == flip_idx) {
+    //                 diff += coupling;
+    //             }
+    //         }
+    //     }
+    // }
+    // return diff;
+}
+
 int qubo_size(const qubo_t& Q) {
     int n = 0;
     for (const auto& entry : Q) {
@@ -77,9 +138,12 @@ result sim_anneal(const qubo_t& Q, const settings s) { // intentionally get copy
     for (int iter = 0; iter < s.max_iter; iter++) {
         solution_t x_prime = x;
         int flip_idx = flip_dis(gen);
+
+        double f_x_prime = evaluateDiff(x, Q, flip_idx) + f_x;
+
         x_prime[flip_idx] = !x_prime[flip_idx];
 
-        double f_x_prime = evaluate(x_prime, Q);
+        // double f_x_prime = evaluate(x_prime, Q);
 
         if (f_x_prime < f_x || dis(gen) < exp((f_x - f_x_prime) / T)) {
             x = x_prime;
@@ -89,17 +153,10 @@ result sim_anneal(const qubo_t& Q, const settings s) { // intentionally get copy
         if (f_x < best_f_x) {
             best_x = x;
             best_f_x = f_x;
-            // cout << "New best energy: " << best_f_x << endl;
-            // cout << "New best solution: ";
-            // for (auto xi : best_x) {
-            //     cout << xi << " ";
-            // }
         }
 
         T = s.temp_scheduler(s.T_0, T, iter, s.max_iter);
     }
-
-    // cout << "Best energy: " << best_f_x << endl;
 
     return {best_x, best_f_x};
 }
@@ -212,9 +269,11 @@ int main() {
     random_device rd;
     unsigned seed = rd();
 
-    settings s = {.max_iter = 10000, .T_0 = 100.0, .temp_scheduler = make_geometric_scheduler(0.99), .seed = seed};
+    settings s = {.max_iter = 40000, .T_0 = 100.0, .temp_scheduler = make_geometric_scheduler(0.999), .seed = seed};
 
-    vector<result> results = multithreaded_sim_anneal(Q, s, 8, 4);
+    // settings s = {.max_iter = 5, .T_0 = 100.0, .temp_scheduler = linear_scheduler, .seed = 2};
+
+    vector<result> results = multithreaded_sim_anneal(Q, s, 4, 4);
     result best = results[0];
 
     cout << "Best energy: " << best.energy << '\n';
